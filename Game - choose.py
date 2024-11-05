3ac19a11ef69a8413c864c7b65d13dcebf238a6e
Leona-course <lejla.nemcova@seznam.cz>
2024-11-05 07:16:27 +0100

    add chooices

--- Game - choose.py
+++ Game - choose.py
@@ -1,4 +1,11 @@
-najist a napit                      + 10 zdravi + 5 energie
-Oblect znehodnotit jidlo            - 10 zdravi
-jit ven nahy                        - 20 teplo
-spat a byt v krytu                  + 10 teplo -10 zdravi
+#najist a napit                          + 10 zdravi + 5 energie
+#oblect a znehodnotit jidlo              - 10 zdravi - 3 energie
+#jit ven nahy                            - 20 teplo
+#jit ven v triku a kratasech             - 10 teplo
+#jit ven v mikine a kalhotech            - 5 teplo, - 10 zdravi
+#jit ven v bunde a zateplenych kalhotech + 10 zdravi
+#jit ven v kozichu                       + 5 zdravi -2 rychlost
+#spat a byt v krytu                      + 10 teplo -10 zdravi
+#oblekas set                             - 3 energie
+#prohledat kryt + rukavice a cepice      - 5 energie + 10 teplo
+
