de9f8a1d0580eb29e7113ca7ac4cbc4908feff53
Leona-course <lejla.nemcova@seznam.cz>
2024-11-05 10:01:33 +0100

    1-1-1-1... one way missing

--- Game - choose.py
+++ Game - choose.py
@@ -8,4 +8,11 @@
 #spat a byt v krytu                      + 10 teplo -10 zdravi
 #oblekas set                             - 3 energie
 #prohledat kryt + rukavice a cepice      - 5 energie + 10 teplo
-
+#najit vodu                              - 2 energie + 5 zdravi
+#vytvorit si harpunu a ulovit rybu       - 5 energie
+#rozdelat ohen                           - 3 energie + 10 teplo
+#syrova ryba                             + 5 zdravi - 5 teplo
+#ohrata ryba                             + 5 energie + 5 zdravi
+#kryt a ohen                             - 6 energie +10 bezpeci +10 teplo
+#usnout bez ohne                         - 10 bezpeci - 10 teplo
+#usnout venku s ohnem                    - 5 bezpeci + 10 teplo
